0b81d8631353256609babb5c654e6601f72a6550
joshantanu <44172238+joshantanu@users.noreply.github.com>
2022-06-26 22:06:22 +0530

    previous month rate defect fix

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import React, { useEffect, useState } from "react";
-import DatePicker from 'sassy-datepicker';
+import DatePicker from "sassy-datepicker";
 import MonthView from "./components/MonthView";
 import firebase from "./firebase/config";
 //import firebase, {SignInWithFirebase, LogoutWithFirebase} from "./firebase/config";
 import { monthNames } from "./AppConst";
 import Qty from "./components/Qty";
+const db = firebase.firestore();
 
 function App() {
-  const [yearlyData, setyearlyData] = useState([]);
   const [formData, setformData] = useState({ cow: 1, buff: 1 });
   const [date, setDate] = useState(new Date());
 
-  const buildDateObj = thisDate => {
+  const buildDateObj = (thisDate) => {
     const newDate = new Date(thisDate);
     let dmy = {};
     dmy.date = newDate.getDate();
     dmy.month = monthNames[newDate.getMonth()];
     dmy.year = newDate.getFullYear();
-    return { ...dmy }
-  }
+    return { ...dmy };
+  };
   const [selectedDMY, setSelectedDMY] = useState(buildDateObj(date));
   const [monthlyData, setmonthlyData] = useState({});
   const [monthlyRate, setmonthlyRate] = useState({});
- 
-  const db = firebase.firestore();
+
   const fetchData = async () => {
     db.doc(`appDB/${selectedDMY.year}`)
       .get()
       .then((querySnapshot) => {
         let yearlyData = querySnapshot.data();
         //console.log(dailyData)
-        setyearlyData(yearlyData);
         const monthlyData = yearlyData[selectedDMY.month];
-        setmonthlyRate(monthlyData.Rate)
+        monthlyData.Rate = monthlyData.Rate || { cow: 50, buff: 60 };
+        setmonthlyRate(monthlyData.Rate);
         delete monthlyData.Rate;
         setmonthlyData(monthlyData);
       });
@@ -42,10 +41,9 @@ function App() {
   }, [selectedDMY.year]);
 
   const setData = () => {
-    //if(!formData.cow_rate){
-      formData.cow_rate = formData.cow_rate || monthlyRate.cow;
-      formData.buff_rate = formData.buff_rate || monthlyRate.buff;
-    //}
+    formData.cow_rate = formData.cow_rate || monthlyRate.cow;
+    formData.buff_rate = formData.buff_rate || monthlyRate.buff;
+
     db.doc(`appDB/${selectedDMY.year}`)
       .set(
         {
@@ -54,35 +52,33 @@ function App() {
               cow: formData.cow,
               buff: formData.buff,
             },
-            Rate:{
-              cow:formData.cow_rate,
-              buff:formData.buff_rate
-            }
-          }
+            Rate: {
+              cow: formData.cow_rate,
+              buff: formData.buff_rate,
+            },
+          },
         },
         { merge: true }
       )
-      .then((docRef) => {
+      .then(() => {
         alert("Data Successfully Submitted");
         fetchData();
       });
   };
 
   const getFormData = (obj) => {
-    let thisformData = {}
-
-    thisformData.[obj.target.name] = obj.target.value
-     console.log(formData);
-
+    let thisformData = {};
+    thisformData[obj.target.name] = obj.target.value;
+    //console.log(formData);
     setformData({ ...formData, ...thisformData });
-  }
-  const onDateChange = thisDate => {
-    const dmy = buildDateObj(thisDate)
+  };
+
+  const onDateChange = (thisDate) => {
+    const dmy = buildDateObj(thisDate);
     //console.log(thisDate)
     //newDate = ${newDate.getFullYear()} ${monthNames[newDate.getMonth()]} ${newDate.getDate()}`);
     setDate(thisDate);
-    setSelectedDMY(dmy)
-
+    setSelectedDMY(dmy);
   };
 
   // const [isUserSignedIn, setisUserSignedIn] = useState();
@@ -93,36 +89,59 @@ function App() {
   //   setisUserSignedIn(false);
   // })
 
-
   return (
     <>
       <h3>Ashtavinayak Milk App</h3>
       {/* <a href="#" onClick={SignInWithFirebase}>Login</a>
       <a href="#" onClick={LogoutWithFirebase}>Logout</a>
       // {console.log(isUserSignedIn)} |  */}
-  
-      <div className="text-center"><DatePicker onChange={onDateChange} selected={date} maxDate={new Date()} /></div>
+
+      <div className="text-center">
+        <DatePicker
+          onChange={onDateChange}
+          selected={date}
+          maxDate={new Date()}
+        />
+      </div>
       <br />
       {/* {console.log(monthlyData)} */}
-      <table className="table"><tbody>
-        <tr>
-          <th>Cow</th>
-          <th>Buffello</th>
-        </tr>
-        <tr>
-          <td>
-           <Qty milkType="cow" controlClick={getFormData} rate={monthlyRate} />
-          </td>
-          <td>
-          <Qty milkType="buff" controlClick={getFormData} rate={monthlyRate} />
-          </td>
-        </tr></tbody>
+      <table className="table">
+        <tbody>
+          <tr>
+            <th>Cow</th>
+            <th>Buffello</th>
+          </tr>
+          <tr>
+            <td>
+              <Qty
+                milkType="cow"
+                controlClick={getFormData}
+                rate={monthlyRate}
+              />
+            </td>
+            <td>
+              <Qty
+                milkType="buff"
+                controlClick={getFormData}
+                rate={monthlyRate}
+              />
+            </td>
+          </tr>
+        </tbody>
       </table>
       <div className="text-center">
-        <button onClick={setData} className="btn btn-primary">SUBMIT</button>
+        <button onClick={setData} className="btn btn-primary">
+          SUBMIT
+        </button>
       </div>
-      <div><br /></div>
-      <MonthView monthData={monthlyData} rateCow={monthlyRate.cow} rateBuff={monthlyRate.buff} />
+      <div>
+        <br />
+      </div>
+      <MonthView
+        monthData={monthlyData}
+        rateCow={monthlyRate.cow}
+        rateBuff={monthlyRate.buff}
+      />
     </>
   );
 }
